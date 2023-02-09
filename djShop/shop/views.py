fb821939e6944bdeb3f4c5136c4a2117e3e8190d
Suryana <nanasuryana335@gmail.com>
2023-02-09 20:32:29 +0700

    fix: fix several things that contains bugs or wrong code
    Here are the code that i've fix:
    
    [1] Initialize category_slug = None instead of just declaration of
        category_slug on "product_list" parameter that causing error
    [2] Change products filtering (when category_slug is true) from search
        with objects manager to use existing products variable
    [3] Give additional parameter when filtering model object field
        with get_object_or_404 when "available=True"

--- djShop/shop/views.py
+++ djShop/shop/views.py
@@ -4,13 +4,13 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 
-def product_list(request, category_slug):
+def product_list(request, category_slug= None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=category)
+        products = products.filter(category=category)
 
     return render(
         request,
@@ -24,7 +24,7 @@ def product_list(request, category_slug):
 
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug)
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(
         request,
         'shop/product/detail.html',
