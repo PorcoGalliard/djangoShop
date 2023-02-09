bfa3f488622d019fb7feaa71b1c43a63e175cf2f
Suryana <nanasuryana335@gmail.com>
2023-02-09 16:21:06 +0700

    feat: create product_list and product_detail views

--- djShop/shop/views.py
+++ djShop/shop/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from .models import Product, Category
+
+
+def product_list(request, category_slug):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category)
+
+    return render(
+        request,
+        'shop/product/list.html',
+        {
+            'category': category,
+            'products': products,
+            'categories': categories,
+        }
+    )
+
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug)
+    return render(
+        request,
+        'shop/product/detail.html',
+        {
+            'product': product,
+        }
+    )
