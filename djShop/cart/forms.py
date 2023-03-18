ec19ece45d9e7e4421e2c2ebb74d68a65cad61bb
Suryana <nanasuryana335@gmail.com>
2023-03-18 21:55:40 +0700

    fix: fix error code on override field

--- djShop/cart/forms.py
+++ djShop/cart/forms.py
@@ -4,7 +4,9 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
+    # coerce used to convert the input to integer
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,
                                       coerce=int)
+    # overriding mean create new amount for the product
     override = forms.BooleanField(
-        required=False, initial=False, widget=HiddenInput)
+        required=False, initial=False, widget=forms.HiddenInput)
